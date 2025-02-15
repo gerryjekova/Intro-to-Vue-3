0cefbad9b2a8e77f8ea3dcd947ca3f3970cf8bfe
Gerry <155331890+NqmaNazad@users.noreply.github.com>
2025-02-15 07:51:33 +0200

    Creating the Vue App

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 > [To Find Vue Script from CDN](https://vuejs.org/guide/quick-start.html#using-vue-from-cdn)
->> 
+>> using mountedApp.product = 'Shoes'  in F12 Console changes the data and updates it automatically
+>>>![alt text](assets/images/update-devtools.png)
--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-//create app
+// 1 create app  
+// 2 ({})  <- OPTIONS OBJECT - allows addition of optional properties in order to config the application 
+// !! -- It needs to be included even if empty if you want to get it started
+// It creates the Vue instance which is the 💗 of our APP - it powers everything
 const app = Vue.createApp({
-  //declare function() { return {} }
+  //data OPTION
     data() {
+        // returns an object
         return {
-            product: 'Socks'
+            // we place our data inside here, which for now is just Socks
+            product: 'Lamp '
+            // we then proceed to import the app in our index.html <div id="app"> <h1> {{ product }} </h1> </div>
         }
     }
 })
